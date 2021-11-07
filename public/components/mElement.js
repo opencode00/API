06f11f70ac49c7da0eecedd44e646750a89cd6e5
Pedro Hdez <opencode00@gmail.com>
2021-11-07 19:49:56 +0000

    math element

--- public/components/mElement.js
+++ public/components/mElement.js
@@ -2,22 +2,24 @@ const tpl = document.createElement('template');
 tpl.innerHTML = `
     <style>
         #element{
+            font-family: Console;
             display: flex;
             color: #ffcb00;
             width: max-content;
-            border: 8px solid #ffcb00;
-            font-size: 48px; 
-            padding:40px 4px; 
+            border: **quartersize** solid #ffcb00;
+            font-size: **size**; 
+            padding: 1em 0.1em; 
         }
         #superior{
             margin-top: 0.1em;
-            font-size:24px; 
+            font-size: **halfsize**;
             margin-left: 0em;
+            margin-right: 1em;
         }
         #inferior{
-            margin-top: 1em; 
-            font-size:24px; 
-            margin-left: -0.9em; 
+            margin-top: **90size**; 
+            font-size: **halfsize**; 
+            margin-left: -2.1em; 
             padding-right: 0.1em;
         }
     </style>
@@ -30,29 +32,33 @@ tpl.innerHTML = `
 class mElement extends HTMLElement{
     constructor(){
         super()
-        this.attachShadow({mode: 'open'});
+        if (!this.hasAttribute('size')) this.setAttribute('size', 2)
+        tpl.innerHTML = tpl.innerHTML.toString().replaceAll('**size**;', this.getAttribute('size')+'em;')
+        tpl.innerHTML = tpl.innerHTML.toString().replaceAll('**halfsize**;', this.getAttribute('size')/8+'em;')
+        tpl.innerHTML = tpl.innerHTML.toString().replaceAll('**quartersize**', this.getAttribute('size')/24+'em')
+        tpl.innerHTML = tpl.innerHTML.toString().replaceAll('**90size**', ((this.getAttribute('size')/2)*100)/100+'em')
         const content = tpl.content.cloneNode(true);
+        this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(content);
         this.update();
     }   
-    // get superior() { return this.getAttribute('superior') }
-    // set superior(sVal) {this.setAttribute('superior', sVal) }
-    
-    // get inferior() { return this.getAttribute('inferior') }
-    // set inferior(iVal) {this.setAttribute('inferior', iVal) }
-    
-    // attributeChangedCallback(name, oldVal, newVal){
-    //     this.update();
-    // }
-
+   
     connectedCallback(){
         this.update();
     }
 
+    static get observedAttributes() {
+        return ['superior','inferior']
+    } 
+    
+    attributeChangedCallback(name, oldVal, newVal){
+        this.update()
+    }
+
     update(){
-        // this.shadowRoot.innerHTML="DR";
+       
         this.shadowRoot.querySelector('#superior').innerHTML = this.getAttribute('superior');
-        this.shadowRoot.querySelector('#inferior').innerHTML = this.getAttributre('inferior');
+        this.shadowRoot.querySelector('#inferior').innerHTML = this.getAttribute('inferior');
     }
 }
 customElements.define('math-element', mElement);
